fd1f16c1b07063890d2c806f7bb4cf3960631fea
Austin Goergen <syunamasigaming@gmail.com>
2022-01-19 14:04:18 -0800

    Created basic window for img_viewer

---
+++ img_viewer/img_viewer.c
@@ -0,0 +1,33 @@
+#define UI_FILE "img_viewer.glade"
+
+#include <gtk/gtk.h>
+
+int main(int argc, char **argv)
+{
+    GtkWidget *main_window;
+    GtkWidget *image;
+    GtkBuilder *builder;
+
+    //Init GTK+
+    gtk_init(&argc, &argv);
+
+    //Create new GtkBuilder object from file
+    builder = gtk_builder_new_from_file(UI_FILE);
+
+    main_window = GTK_WIDGET(gtk_builder_get_object(builder, "main_window"));
+    image = GTK_WIDGET(gtk_builder_get_object(builder, "image"));
+
+    // Connect signals
+    gtk_builder_connect_signals(builder, NULL);
+    
+    // Destroy builder, since we don't need it anymore
+    g_object_unref(G_OBJECT(builder));
+
+    // Show window.  All other widgets are automatically shown by GtkBuilder
+    gtk_widget_show(main_window);
+
+    // Start main loop
+    gtk_main();
+
+    return(0);
+}
